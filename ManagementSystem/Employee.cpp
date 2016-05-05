ad0ec49a60194a67ff2a3fe742afca7f155a32ab
Lily <408310996@qq.com>
2016-05-05 22:51:54 +0800

    fix a bug

--- ManagementSystem/Employee.cpp
+++ ManagementSystem/Employee.cpp
@@ -43,12 +43,11 @@ BOOL CEmployee::OnInitDialog()
 
 	// TODO:  在此添加额外的初始化
 	//连接数据库
-	//string ConstrWindows = "server=8PDYMQCADPLRIDS;database=MyData;Truster_Connection=SSPI";
-
 	CRect rc;
 	m_ListCtrl.GetWindowRect(&rc);
 	//LVS_EX_GRIDLINES是希望显示网格；LVS_EX_FULLROWSELECT是希望被选中时整行
 	//LVS_EX_HEADERDRAGDROP是让其支持点击表头排序；LVS_EX_TWOCLICKACTIVATE是希望有鼠标在未被选中的行上移动的时候有一些效果 
+//	m_ListCtrl.InsertColumn(0,_T("序号"),LVCFMT_CENTER,rc.Size().cx/4,0);
 	m_ListCtrl.InsertColumn(0,_T("工号"),LVCFMT_CENTER,rc.Size().cx/3,0);
 	m_ListCtrl.InsertColumn(1,_T("姓名"),LVCFMT_CENTER,rc.Size().cx/3,1);
 	m_ListCtrl.InsertColumn(2,_T("联系号码"),LVCFMT_CENTER,rc.Size().cx/3,2);
@@ -72,21 +71,23 @@ void CEmployee::OnBnClickedBtnQuit()
 void CEmployee::OnBnClickedBtnShow()
 {
 	// TODO: 在此添加控件通知处理程序代码
-	bool i=m_DataBase.Open();
+	bool t=m_DataBase.Open();
 
-	if( i==true )
+	if( t==true )
 	{
 		vector<_variant_t> vName;
+		vName.push_back("ID");
+		vName.push_back("NAME");
+		vName.push_back("Phone");
 		vector<vector<_variant_t> > vResult(m_DataBase.Select(::SysAllocString(L"SELECT * FROM Employee"),vName));
 		m_ListCtrl.DeleteAllItems();
-		for(int i=0;i<vName.size();++i)
+		for(int i=0; i<vResult.size(); ++i )
 		{
-			m_ListCtrl.InsertItem( i,    m_DataBase.VariantToCString(vResult.at(i).at(0)));
-			m_ListCtrl.SetItemText(i, 1, m_DataBase.VariantToCString(vResult.at(i).at(0)) );
+			m_ListCtrl.InsertItem(i,  m_DataBase.VariantToCString(vResult.at(i).at(0)) );
+			m_ListCtrl.SetItemText(i, 1, m_DataBase.VariantToCString(vResult.at(i).at(1)) ); 
 			m_ListCtrl.SetItemText(i, 2, m_DataBase.VariantToCString(vResult.at(i).at(2)) ); 
-			m_ListCtrl.SetItemText(i, 3, m_DataBase.VariantToCString(vResult.at(i).at(3)) ); 
 		}
-		m_DataBase.Close();
+	//	m_DataBase.Close();// 会报错？？
 	}
 	else
 	{
